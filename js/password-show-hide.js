0aeabf1d3117d4add3478e61aa8c591c77627522
[sachinimalsha] <[mhsm.dulakshi@gmail.com]>
2022-07-20 13:43:12 +0530

    password show/hide

---
+++ js/password-show-hide.js
@@ -0,0 +1,12 @@
+const pswrdFeild = document.querySelector(".form input[type = 'password']"),
+    toggleBtn = document.querySelector(".form .field i");
+
+toggleBtn.onclick = () => {
+    if (pswrdFeild.type == 'password') {
+        pswrdFeild.type = "text";
+        toggleBtn.classList.add("active");
+    } else {
+        pswrdFeild.type = "password";
+        toggleBtn.classList.remove("active");
+    }
+}
